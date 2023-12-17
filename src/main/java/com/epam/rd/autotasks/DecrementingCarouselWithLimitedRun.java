a282d00540e94c3b40f93584cabd2dd8229e71f4
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 20:49:49 +0100

    decrementing-carousel-with-limit_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
@@ -2,47 +2,48 @@ package com.epam.rd.autotasks;
 
 class DecrementingCarouselWithLimitedRun extends DecrementingCarousel {
 
-    private final int actionLimit;
+  private final int actionLimit;
 
-    public DecrementingCarouselWithLimitedRun(final int capacity, final int actionLimit) {
-        super(capacity);
-        this.actionLimit = actionLimit;
+  public DecrementingCarouselWithLimitedRun(final int capacity, final int actionLimit) {
+    super(capacity);
+    this.actionLimit = actionLimit;
+  }
+
+  @Override
+  public CarouselRun run() {
+    if (!isRun) {
+      isRun = true;
+      return new DecrementingCarouselWithLimitRun();
+    }
+    return null;
+  }
+
+  public class DecrementingCarouselWithLimitRun extends CarouselRun {
+
+    private final int decrement = 1;
+    private int actionsTaken = 0;
+
+    @Override
+    public int next() {
+      if (isFinished() || actionsTaken >= actionLimit) {
+        return -1;
+      }
+
+      int beforeDecreasing = array[position];
+      array[position] -= decrement;
+      actionsTaken++;
+
+      do {
+        position++;
+      } while (array[position %= array.length] <= 0 && !isFinished());
+
+      return beforeDecreasing;
     }
 
     @Override
-    public CarouselRun run() {
-        if (!isRun) {
-            isRun = true;
-            return new DecrementingCarouselWithLimitRun();
-        }
-        return null;
+    public boolean isFinished() {
+      return super.isFinished() || actionsTaken >= actionLimit;
     }
-
-    public class DecrementingCarouselWithLimitRun extends CarouselRun {
-        private int decrement = 1;
-        private int actionsTaken = 0;
-
-        @Override
-        public int next() {
-            if (isFinished() || actionsTaken >= actionLimit) {
-                return -1;
-            }
-
-            int beforeDecreasing = array[position];
-            array[position] -= decrement;
-            actionsTaken++;
-
-            do {
-                position++;
-            } while (array[position %= array.length] <= 0 && !isFinished());
-
-            return beforeDecreasing;
-        }
-
-        @Override
-        public boolean isFinished() {
-            return super.isFinished() || actionsTaken >= actionLimit;
-        }
-    }
+  }
 
 }
